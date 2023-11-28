c13bb0af8e7483307275c3d10e8ae4030b781354
Willem <willemhumphrey@gmail.com>
2023-11-28 00:11:47 -0500

    implement logic inside toHash and compare static functions

--- src/services/password.ts
+++ src/services/password.ts
@@ -1,5 +1,20 @@
-export class Password {
-  static toHash(password: string) {}
+import { scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
 
-  static compare(storedPassword: string, suppliedPassword: string) {}
+const scryptAsync = promisify(scrypt);
+
+export class Password {
+  static async toHash(password: string) {
+    const salt = randomBytes(8).toString('hex');
+    const buffer = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    return `${buffer.toString('hex')}.${salt}`;
+  }
+
+  static async compare(storedPassword: string, suppliedPassword: string) {
+    const [hashedPassword, salt] = storedPassword.split('.');
+    const buffer = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buffer.toString('hex') === hashedPassword;
+  }
 }
