fce459c46647954c05eb47e32f3793d3c2d28863
Willem <willemhumphrey@gmail.com>
2023-11-29 14:53:46 -0500

    add pre save function to check password

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { Password } from '../services/password';
 
 // an interface that describes properties required to create a new user
 interface UserAttrs {
@@ -26,6 +27,15 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+
+  next();
 });
 
 userSchema.statics.build = (attrs: UserAttrs) => {
