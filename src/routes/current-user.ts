0353131d0eb17bcced15757056255f2e06c3cd5e
Willem <willemhumphrey@gmail.com>
2024-01-05 08:54:06 -0500

    check for user stored jwt cookie in currentUser

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,10 +1,21 @@
-import express from "express";
+import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  console.log("test currentuser");
-  res.send("hello there");
+router.get('/api/users/currentuser', (req, res) => {
+  // console.log(req.session);
+
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
