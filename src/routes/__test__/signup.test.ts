8bed921d0d33687d207b13b8f9c4002e18c5a3e0
Willem <willemhumphrey@gmail.com>
2024-01-12 18:51:43 -0500

    writes tests that return status 400 on incorrect data

--- src/routes/__test__/signup.test.ts
+++ src/routes/__test__/signup.test.ts
@@ -10,3 +10,34 @@ it('returns a 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns 400 with invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns 400 with invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@',
+      password: '1',
+    })
+    .expect(400);
+});
+
+it('returns 400 with missing email or password password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: '', password: '1' })
+    .expect(400);
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'test@test.com', password: '' })
+    .expect(400);
+});
